588b94afbc9a3cd2ddbbb649b0672de493943fc0
tandung1512 <tandung15122003@gmail.com>
2024-10-26 15:40:46 +0700

    add login response

--- src/main/java/web/controller/AuthController.java
+++ src/main/java/web/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import web.model.Account;
 import web.model.LoginRequest;
+import web.model.responses.LoginResponse;
 import web.service.AccountService;
 
 import java.util.Optional;
@@ -36,7 +37,19 @@ public class AuthController {
             // So sánh mật khẩu đã mã hóa với mật khẩu người dùng nhập vào
             if (passwordEncoder.matches(loginRequest.getPassword(), account.getPassword())) {
                 // Trả về thông tin tài khoản đầy đủ nếu đăng nhập thành công
-                return ResponseEntity.ok(account);
+            	
+            	// tạo token bằng jwt
+            	
+            	// tạo mới loginResponse object
+            	LoginResponse data = new LoginResponse();
+            	data.id= account.getId();
+            	data.name= account.getName();
+            	data.phone= account.getPhone();
+            	data.email=account.getEmail();
+            	data.address = account.getAddress();
+            	data.token= "";
+            	
+                return ResponseEntity.ok(data);
             }
         }
 
---
+++ src/main/java/web/model/responses/LoginResponse.java
@@ -0,0 +1,16 @@
+package web.model.responses;
+public class LoginResponse {
+	public String id;
+
+	public String name;
+
+
+	public String phone;
+
+
+	public String email;
+
+	public String address;
+	public String image;
+	public String token;
+}
